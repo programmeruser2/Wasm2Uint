5d7c9b3122fc4a768acfcdb24015c49dfc6d95d1
TheProgrammer3 <firesoldier999@gmail.com>
2020-10-15 19:14:01 +0000

    Add MIME type check

--- converter.js
+++ converter.js
@@ -3,13 +3,19 @@ const fileURLInput = document.getElementById('wasm-file-url');
 
 async function wasmFromURL() {
   const url = fileURLInput.value;
-  if(url.trim()) {
+  if (url.trim()) {
     const result = await convertWasmFromURLToUint(url);
     outputArea.value = `var wasmCode = new Uint8Array([${result}])`;
   }
 }
 async function convertWasmFromURLToUint(url) {
-  const req = await fetch('https://cors-anywhere.herokuapp.com/'+url);
-  const bytes = await req.arrayBuffer();
+  const res = await fetch('https://cors-anywhere.herokuapp.com/'+url);
+	if (
+		res.headers.get('Content-Type').indexOf('application/wasm') === -1 && 
+		!(window.confirm(`This file doesn\'t appear to be a WebAssembly file.\n Do you want to load the file at ${url}?`)) 
+	) {
+		return;
+	}
+  const bytes = await res.arrayBuffer();
   return new Uint8Array(bytes);
 }
