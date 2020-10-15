d1838b41858112a8eecac9c37b020e508d7472c9
icositetrachoron-programmer <69394951+icositetrachoron-programmer@users.noreply.github.com>
2020-10-15 12:36:52 -0400

    Update converter.js

--- converter.js
+++ converter.js
@@ -1,6 +1,6 @@
 function convertWasmFromURLToUint(url) {
   let result;
-  fetch(url)
+  return fetch(url)
   .then(response => response.arrayBuffer())
   .then(bytes => return new Uint8Array(response))
 }
