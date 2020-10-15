687c99665bb3991ad44ff7b5c232664a0ec97332
icositetrachoron-programmer <69394951+icositetrachoron-programmer@users.noreply.github.com>
2020-10-15 13:45:49 -0400

    Update converter.js

--- converter.js
+++ converter.js
@@ -1,3 +1,10 @@
+const outputArea = document.getElementById('output');
+const fileURLInput = document.getElementById('wasm-file-url);
+
+async function wasmFromURL() {
+  const result = await convertWasmFromURL(fileURLInput.value);
+  outputArea.value = result;
+}
 async function convertWasmFromURLToUint(url) {
   const req = await fetch(url);
   const bytes = await req.arrayBuffer();
