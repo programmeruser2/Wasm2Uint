9defa717b25cefcd71baa8dbb5ae8c78d7b19263
icositetrachoron-programmer <69394951+icositetrachoron-programmer@users.noreply.github.com>
2020-10-15 11:28:46 -0400

    Update converter.js

--- converter.js
+++ converter.js
@@ -2,5 +2,5 @@ async function convertWasmFromURL(url) {
   const req = await fetch(url);
   const wasmArrayBuffer = await req.arrayBuffer();
   const result = await new Uint8Array(wasmArrayBuffer);
-  return result;
+  await return result;
 }
