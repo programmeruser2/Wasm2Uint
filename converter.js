eca90d3628ac4e688937b089a5b537fad39f32f2
icositetrachoron-programmer <69394951+icositetrachoron-programmer@users.noreply.github.com>
2020-10-15 11:44:51 -0400

    Update converter.js

--- converter.js
+++ converter.js
@@ -1,5 +1,7 @@
 function convertWasmFromURL(url) {
+  let result;
   fetch(url)
   .then(response => response.arrayBuffer())
-  .then(bytes => return new Uint8Array(bytes));
+  .then(bytes => { result = new Uint8Array(bytes) });
+  return result;
 }
